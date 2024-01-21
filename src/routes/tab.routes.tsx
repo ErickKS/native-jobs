ebd894f89135488019b008bb4e559f704bbaeab6
Erick <erick.kuwahara79@gmail.com>
2024-01-21 17:45:25 -0300

    add: initial files

---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,41 @@
+import { BottomTabNavigationOptions, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Feather } from "@expo/vector-icons";
+
+import Home from "../screens/home";
+import Search from "../screens/search";
+
+const Tab = createBottomTabNavigator();
+
+const screenOptions: BottomTabNavigationOptions = {
+  headerShown: false,
+  tabBarStyle: {
+    backgroundColor: "#09090A",
+    borderTopWidth: 0,
+  },
+  tabBarActiveTintColor: "#8b5cf6",
+};
+
+export default function TabRoutes() {
+  return (
+    <Tab.Navigator {...{ screenOptions }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name="home" color={color} size={20} />,
+          tabBarLabel: "Home",
+        }}
+      />
+
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => <Feather name="plus" color={color} size={20} />,
+          tabBarLabel: "Search",
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
---
+++ src/screens/search.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Search() {
+  return (
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-white">Search</Text>
+    </View>
+  );
+}
